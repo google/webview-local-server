9c2439689cd0ba900b222c0b49e92319ac1a931b
Marcin Kosiba <mkosiba@google.com>
2015-01-15 11:13:16 +0000

    Fork UriMatcher.java

--- src/main/third_party/android/java/com/google/webviewlocalserver/third_party/android/UriMatcher.java
+++ src/main/third_party/android/java/com/google/webviewlocalserver/third_party/android/UriMatcher.java
@@ -13,8 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
-package android.content;
+package com.google.webviewlocalserver.third_party.android;
 
 import android.net.Uri;
 
@@ -22,112 +21,14 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Pattern;
 
-/**
-Utility class to aid in matching URIs in content providers.
-
-<p>To use this class, build up a tree of <code>UriMatcher</code> objects.
-For example:
-<pre>
-    private static final int PEOPLE = 1;
-    private static final int PEOPLE_ID = 2;
-    private static final int PEOPLE_PHONES = 3;
-    private static final int PEOPLE_PHONES_ID = 4;
-    private static final int PEOPLE_CONTACTMETHODS = 7;
-    private static final int PEOPLE_CONTACTMETHODS_ID = 8;
-
-    private static final int DELETED_PEOPLE = 20;
-
-    private static final int PHONES = 9;
-    private static final int PHONES_ID = 10;
-    private static final int PHONES_FILTER = 14;
-
-    private static final int CONTACTMETHODS = 18;
-    private static final int CONTACTMETHODS_ID = 19;
-
-    private static final int CALLS = 11;
-    private static final int CALLS_ID = 12;
-    private static final int CALLS_FILTER = 15;
-
-    private static final UriMatcher sURIMatcher = new UriMatcher(UriMatcher.NO_MATCH);
-
-    static
-    {
-        sURIMatcher.addURI("contacts", "people", PEOPLE);
-        sURIMatcher.addURI("contacts", "people/#", PEOPLE_ID);
-        sURIMatcher.addURI("contacts", "people/#/phones", PEOPLE_PHONES);
-        sURIMatcher.addURI("contacts", "people/#/phones/#", PEOPLE_PHONES_ID);
-        sURIMatcher.addURI("contacts", "people/#/contact_methods", PEOPLE_CONTACTMETHODS);
-        sURIMatcher.addURI("contacts", "people/#/contact_methods/#", PEOPLE_CONTACTMETHODS_ID);
-        sURIMatcher.addURI("contacts", "deleted_people", DELETED_PEOPLE);
-        sURIMatcher.addURI("contacts", "phones", PHONES);
-        sURIMatcher.addURI("contacts", "phones/filter/*", PHONES_FILTER);
-        sURIMatcher.addURI("contacts", "phones/#", PHONES_ID);
-        sURIMatcher.addURI("contacts", "contact_methods", CONTACTMETHODS);
-        sURIMatcher.addURI("contacts", "contact_methods/#", CONTACTMETHODS_ID);
-        sURIMatcher.addURI("call_log", "calls", CALLS);
-        sURIMatcher.addURI("call_log", "calls/filter/*", CALLS_FILTER);
-        sURIMatcher.addURI("call_log", "calls/#", CALLS_ID);
-    }
-</pre>
-<p>Starting from API level {@link android.os.Build.VERSION_CODES#JELLY_BEAN_MR2}, paths can start
- with a leading slash.  For example:
-<pre>
-        sURIMatcher.addURI("contacts", "/people", PEOPLE);
-</pre>
-<p>Then when you need to match against a URI, call {@link #match}, providing
-the URL that you have been given.  You can use the result to build a query,
-return a type, insert or delete a row, or whatever you need, without duplicating
-all of the if-else logic that you would otherwise need.  For example:
-<pre>
-    public String getType(Uri url)
-    {
-        int match = sURIMatcher.match(url);
-        switch (match)
-        {
-            case PEOPLE:
-                return "vnd.android.cursor.dir/person";
-            case PEOPLE_ID:
-                return "vnd.android.cursor.item/person";
-... snip ...
-                return "vnd.android.cursor.dir/snail-mail";
-            case PEOPLE_ADDRESS_ID:
-                return "vnd.android.cursor.item/snail-mail";
-            default:
-                return null;
-        }
-    }
-</pre>
-instead of:
-<pre>
-    public String getType(Uri url)
-    {
-        List<String> pathSegments = url.getPathSegments();
-        if (pathSegments.size() >= 2) {
-            if ("people".equals(pathSegments.get(1))) {
-                if (pathSegments.size() == 2) {
-                    return "vnd.android.cursor.dir/person";
-                } else if (pathSegments.size() == 3) {
-                    return "vnd.android.cursor.item/person";
-... snip ...
-                    return "vnd.android.cursor.dir/snail-mail";
-                } else if (pathSegments.size() == 3) {
-                    return "vnd.android.cursor.item/snail-mail";
-                }
-            }
-        }
-        return null;
-    }
-</pre>
-*/
 public class UriMatcher
 {
-    public static final int NO_MATCH = -1;
     /**
      * Creates the root node of the URI tree.
      *
      * @param code the code to match for the root URI
      */
-    public UriMatcher(int code)
+    public UriMatcher(Object code)
     {
         mCode = code;
         mWhich = -1;
@@ -137,7 +38,7 @@ public class UriMatcher
 
     private UriMatcher()
     {
-        mCode = NO_MATCH;
+        mCode = null;
         mWhich = -1;
         mChildren = new ArrayList<UriMatcher>();
         mText = null;
@@ -150,7 +51,7 @@ public class UriMatcher
      * numbers.
      * <p>
      * Starting from API level {@link android.os.Build.VERSION_CODES#JELLY_BEAN_MR2},
-     * this method will accept leading slash in the path.
+     * this method will accept a leading slash in the path.
      *
      * @param authority the authority to match
      * @param path the path to match. * may be used as a wild card for
@@ -158,10 +59,10 @@ public class UriMatcher
      * @param code the code that is returned when a URI is matched
      * against the given components. Must be positive.
      */
-    public void addURI(String authority, String path, int code)
+    public void addURI(String scheme, String authority, String path, Object code)
     {
-        if (code < 0) {
-            throw new IllegalArgumentException("code " + code + " is invalid: it must be positive");
+        if (code == null) {
+            throw new IllegalArgumentException("Code can't be null");
         }
 
         String[] tokens = null;
@@ -176,8 +77,14 @@ public class UriMatcher
 
         int numTokens = tokens != null ? tokens.length : 0;
         UriMatcher node = this;
-        for (int i = -1; i < numTokens; i++) {
-            String token = i < 0 ? authority : tokens[i];
+        for (int i = -2; i < numTokens; i++) {
+            String token;
+            if (i == -2)
+                token = scheme;
+            else if (i == -1)
+                token = authority;
+            else
+                token = tokens[i];
             ArrayList<UriMatcher> children = node.mChildren;
             int numChildren = children.size();
             UriMatcher child;
@@ -192,8 +99,8 @@ public class UriMatcher
             if (j == numChildren) {
                 // Child not found, create it
                 child = new UriMatcher();
-                if (token.equals("#")) {
-                    child.mWhich = NUMBER;
+                if (token.equals("**")) {
+                    child.mWhich = REST;
                 } else if (token.equals("*")) {
                     child.mWhich = TEXT;
                 } else {
@@ -214,10 +121,10 @@ public class UriMatcher
      *
      * @param uri       The url whose path we will match against.
      *
-     * @return  The code for the matched node (added using addURI), 
-     * or -1 if there is no matched node.
+     * @return  The code for the matched node (added using addURI),
+     * or null if there is no matched node.
      */
-    public int match(Uri uri)
+    public Object match(Uri uri)
     {
         final List<String> pathSegments = uri.getPathSegments();
         final int li = pathSegments.size();
@@ -228,8 +135,14 @@ public class UriMatcher
             return this.mCode;
         }
 
-        for (int i=-1; i<li; i++) {
-            String u = i < 0 ? uri.getAuthority() : pathSegments.get(i);
+        for (int i=-2; i<li; i++) {
+            String u;
+            if (i == -2)
+                u = uri.getScheme();
+            else if (i == -1)
+                u = uri.getAuthority();
+            else
+                u = pathSegments.get(i);
             ArrayList<UriMatcher> list = node.mChildren;
             if (list == null) {
                 break;
@@ -238,33 +151,25 @@ public class UriMatcher
             int lj = list.size();
             for (int j=0; j<lj; j++) {
                 UriMatcher n = list.get(j);
-          which_switch:
+                which_switch:
                 switch (n.mWhich) {
                     case EXACT:
                         if (n.mText.equals(u)) {
                             node = n;
                         }
                         break;
-                    case NUMBER:
-                        int lk = u.length();
-                        for (int k=0; k<lk; k++) {
-                            char c = u.charAt(k);
-                            if (c < '0' || c > '9') {
-                                break which_switch;
-                            }
-                        }
-                        node = n;
-                        break;
                     case TEXT:
                         node = n;
                         break;
+                    case REST:
+                        return n.mCode;
                 }
                 if (node != null) {
                     break;
                 }
             }
             if (node == null) {
-                return NO_MATCH;
+                return null;
             }
         }
 
@@ -272,10 +177,10 @@ public class UriMatcher
     }
 
     private static final int EXACT = 0;
-    private static final int NUMBER = 1;
-    private static final int TEXT = 2;
+    private static final int TEXT = 1;
+    private static final int REST = 2;
 
-    private int mCode;
+    private Object mCode;
     private int mWhich;
     private String mText;
     private ArrayList<UriMatcher> mChildren;

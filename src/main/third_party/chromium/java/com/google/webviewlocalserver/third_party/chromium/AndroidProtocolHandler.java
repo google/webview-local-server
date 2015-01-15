47f126245ef1a9d285b23a41e81f4983311fd952
Marcin Kosiba <mkosiba@google.com>
2015-01-15 11:16:28 +0000

    Fork AndroidProtocolHandler

--- src/main/third_party/chromium/java/com/google/webviewlocalserver/third_party/chromium/AndroidProtocolHandler.java
+++ src/main/third_party/chromium/java/com/google/webviewlocalserver/third_party/chromium/AndroidProtocolHandler.java
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-package org.chromium.android_webview;
+package com.google.webviewlocalserver.third_party.chromium;
 
 import android.content.Context;
 import android.content.res.AssetManager;
@@ -10,89 +10,31 @@ import android.net.Uri;
 import android.util.Log;
 import android.util.TypedValue;
 
-import org.chromium.base.CalledByNative;
-import org.chromium.base.JNINamespace;
-
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.URLConnection;
 import java.util.List;
 
-/**
- * Implements the Java side of Android URL protocol jobs.
- * See android_protocol_handler.cc.
- */
-@JNINamespace("android_webview")
+
 public class AndroidProtocolHandler {
     private static final String TAG = "AndroidProtocolHandler";
 
-    // Supported URL schemes. This needs to be kept in sync with
-    // clank/native/framework/chrome/url_request_android_job.cc.
-    private static final String FILE_SCHEME = "file";
-    private static final String CONTENT_SCHEME = "content";
+    private Context context;
 
-    /**
-     * Open an InputStream for an Android resource.
-     * @param context The context manager.
-     * @param url The url to load.
-     * @return An InputStream to the Android resource.
-     */
-    @CalledByNative
-    public static InputStream open(Context context, String url) {
-        Uri uri = verifyUrl(url);
-        if (uri == null) {
-            return null;
-        }
-        try {
-            String path = uri.getPath();
-            if (uri.getScheme().equals(FILE_SCHEME)) {
-                if (path.startsWith(nativeGetAndroidAssetPath())) {
-                    return openAsset(context, uri);
-                } else if (path.startsWith(nativeGetAndroidResourcePath())) {
-                    return openResource(context, uri);
-                }
-            } else if (uri.getScheme().equals(CONTENT_SCHEME)) {
-                return openContent(context, uri);
-            }
-        } catch (Exception ex) {
-            Log.e(TAG, "Error opening inputstream: " + url);
-        }
-        return null;
+    public AndroidProtocolHandler(Context context) {
+        this.context = context;
     }
 
-    private static int getFieldId(Context context, String assetType, String assetName)
-        throws ClassNotFoundException, NoSuchFieldException, IllegalAccessException {
-        Class<?> d = context.getClassLoader()
-                .loadClass(context.getPackageName() + ".R$" + assetType);
-        java.lang.reflect.Field field = d.getField(assetName);
-        int id = field.getInt(null);
-        return id;
+    public InputStream openAsset(String path) throws IOException {
+        return context.getAssets().open(path, AssetManager.ACCESS_STREAMING);
     }
 
-    private static int getValueType(Context context, int fieldId) {
-        TypedValue value = new TypedValue();
-        context.getResources().getValue(fieldId, value, true);
-        return value.type;
-    }
-
-    private static InputStream openResource(Context context, Uri uri) {
-        assert uri.getScheme().equals(FILE_SCHEME);
+    public InputStream openResource(Uri uri) {
         assert uri.getPath() != null;
-        assert uri.getPath().startsWith(nativeGetAndroidResourcePath());
-        // The path must be of the form "/android_res/asset_type/asset_name.ext".
+        // The path must be of the form ".../asset_type/asset_name.ext".
         List<String> pathSegments = uri.getPathSegments();
-        if (pathSegments.size() != 3) {
-            Log.e(TAG, "Incorrect resource path: " + uri);
-            return null;
-        }
-        String assetPath = pathSegments.get(0);
-        String assetType = pathSegments.get(1);
-        String assetName = pathSegments.get(2);
-        if (!("/" + assetPath + "/").equals(nativeGetAndroidResourcePath())) {
-            Log.e(TAG, "Resource path does not start with " + nativeGetAndroidResourcePath()
-                    + ": " + uri);
-            return null;
-        }
+        String assetType = pathSegments.get(pathSegments.size() - 2);
+        String assetName = pathSegments.get(pathSegments.size() - 1);
+
         // Drop the file extension.
         assetName = assetName.split("\\.")[0];
         try {
@@ -123,99 +65,18 @@ public class AndroidProtocolHandler {
         }
     }
 
-    private static InputStream openAsset(Context context, Uri uri) {
-        assert uri.getScheme().equals(FILE_SCHEME);
-        assert uri.getPath() != null;
-        assert uri.getPath().startsWith(nativeGetAndroidAssetPath());
-        String path = uri.getPath().replaceFirst(nativeGetAndroidAssetPath(), "");
-        try {
-            AssetManager assets = context.getAssets();
-            return assets.open(path, AssetManager.ACCESS_STREAMING);
-        } catch (IOException e) {
-            Log.e(TAG, "Unable to open asset URL: " + uri);
-            return null;
-        }
+    private static int getFieldId(Context context, String assetType, String assetName)
+            throws ClassNotFoundException, NoSuchFieldException, IllegalAccessException {
+        Class<?> d = context.getClassLoader()
+                .loadClass(context.getPackageName() + ".R$" + assetType);
+        java.lang.reflect.Field field = d.getField(assetName);
+        int id = field.getInt(null);
+        return id;
     }
 
-    private static InputStream openContent(Context context, Uri uri) {
-        assert uri.getScheme().equals(CONTENT_SCHEME);
-        try {
-            return context.getContentResolver().openInputStream(uri);
-        } catch (Exception e) {
-            Log.e(TAG, "Unable to open content URL: " + uri);
-            return null;
-        }
+    private static int getValueType(Context context, int fieldId) {
+        TypedValue value = new TypedValue();
+        context.getResources().getValue(fieldId, value, true);
+        return value.type;
     }
-
-    /**
-     * Determine the mime type for an Android resource.
-     * @param context The context manager.
-     * @param stream The opened input stream which to examine.
-     * @param url The url from which the stream was opened.
-     * @return The mime type or null if the type is unknown.
-     */
-    @CalledByNative
-    public static String getMimeType(Context context, InputStream stream, String url) {
-        Uri uri = verifyUrl(url);
-        if (uri == null) {
-            return null;
-        }
-        try {
-            String path = uri.getPath();
-            // The content URL type can be queried directly.
-            if (uri.getScheme().equals(CONTENT_SCHEME)) {
-                return context.getContentResolver().getType(uri);
-                // Asset files may have a known extension.
-            } else if (uri.getScheme().equals(FILE_SCHEME)
-                       && path.startsWith(nativeGetAndroidAssetPath())) {
-                String mimeType = URLConnection.guessContentTypeFromName(path);
-                if (mimeType != null) {
-                    return mimeType;
-                }
-            }
-        } catch (Exception ex) {
-            Log.e(TAG, "Unable to get mime type" + url);
-            return null;
-        }
-        // Fall back to sniffing the type from the stream.
-        try {
-            return URLConnection.guessContentTypeFromStream(stream);
-        } catch (IOException e) {
-            return null;
-        }
-    }
-
-    /**
-     * Make sure the given string URL is correctly formed and parse it into a Uri.
-     * @return a Uri instance, or null if the URL was invalid.
-     */
-    private static Uri verifyUrl(String url) {
-        if (url == null) {
-            return null;
-        }
-        Uri uri = Uri.parse(url);
-        if (uri == null) {
-            Log.e(TAG, "Malformed URL: " + url);
-            return null;
-        }
-        String path = uri.getPath();
-        if (path == null || path.length() == 0) {
-            Log.e(TAG, "URL does not have a path: " + url);
-            return null;
-        }
-        return uri;
-    }
-
-    /**
-     * Set the context to be used for resolving resource queries.
-     * @param context Context to be used, or null for the default application
-     *                context.
-     */
-    public static void setResourceContextForTesting(Context context) {
-        nativeSetResourceContextForTesting(context);
-    }
-
-    private static native void nativeSetResourceContextForTesting(Context context);
-    private static native String nativeGetAndroidAssetPath();
-    private static native String nativeGetAndroidResourcePath();
 }
